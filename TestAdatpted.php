87881b783b535092d3c494c3cd7ed6a311e60b2d
YoriN111 <gere.dominik69@gmail.com>
2024-03-06 15:31:55 +0100

    fixed Test Adapted

--- TestAdatpted.php
+++ TestAdatpted.php
@@ -4,6 +4,6 @@ class TestAdatpted implements DatabaseAdapterInterface
 {
     public function connect(string $string)
     {
-     echo "test interface";
+     echo "test interface asssssssssssssssss";
     }
 }
